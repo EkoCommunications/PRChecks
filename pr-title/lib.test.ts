e092340c22b2e3bf797f0a32c8b825254f3f3ade
Julien Barbay <julien.barbay@gmail.com>
2022-02-11 11:22:41 +0700

    feat: align with conventionalcommits.org spec - description is format free

--- pr-title/lib.test.ts
+++ pr-title/lib.test.ts
@@ -10,14 +10,13 @@ const cases = [
   ['epic: ASC-0000 - foobar', 'pass'], // no scope
   ['epic(sdk): UP-0000 - foobar', 'pass'], // with scope
   ['epic(sdk): ASC-0000 - foobar', 'pass'], // with scope
-  ['epic: UP-0000 - foo-bar+baz_lol', 'pass'], // containing - _
+  ['epic(sdk): UP-0000 - some allowed special chars /._-+&', 'pass'], // special chars
   ['fail: UP-0000 - foobar', 'fail (invalid verb)'],
   ['epic(sdk) UP-0000 foobar', 'fail (no colon)'],
   ['epic(sdk): foobar', 'fail (no ticket number)'],
   ['epic(sdk): 12-XXXX - foobar', 'fail (invalid ticket number)'],
   ['epic(sdk): UP-0000 foobar', 'fail (no separator)'],
   ['epic(sdk): UP-0000 - ', 'fail (no description)'],
-  ['epic(sdk): UP-0000 - :(', 'fail (description invalid chars)'],
   ['epic(sdk): UP-0000 - fix', 'fail (description too short)'],
   [
     'epic(sdk): UP-0000 - this description is exactly 61 chars long which is 1 too much',
